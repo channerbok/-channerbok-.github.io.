2b1524d67e48575e5b0d05ca88ab2c17e281b361
channerbok <92491257+channerbok@users.noreply.github.com>
2022-05-24 15:42:07 -0700

    (4) Commit-Added js and pictures, changes intro

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // JS File  color: #E15D44 ;
+let main = document.querySelector('main');
+let image = document.createElement('img')
+image.src = '/pic1.jpg';
+main.append(image);
+image.style.width = '100%'
+image.style.height = '110%'
+image.id = "about";
 
+let image2 = document.createElement('img')
+image2.src = '/pic2.jpg';
+main.append(image2);
+image2.style.width = '100%'
+image2.style.height = '110%'
+image2.id = "Experience";
